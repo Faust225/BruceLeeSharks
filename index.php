723c0feeeba2c7bcc4125aa3575ba2ca81a803e8
Oskar <oskaras2011@yandex.com>
2019-05-14 09:55:15 +0300

    Maxima shop done

--- index.php
+++ index.php
@@ -1,45 +1,70 @@
-<?php
-
-$random_size = rand(3, 5);
-
-$bruce_lee_sharks = [];
-for ($eile = 1; $eile <= $random_size; $eile++) {
-    $row = [];
-    for ($stulpelis = 1; $stulpelis <= $random_size; $stulpelis++) {
-        $row [] = rand(0, 2);
-    }
-    $bruce_lee_sharks[] = $row;
-}
-
-?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Bruce Lee SHARKS</title>
-        <meta charset="utf-8">
-        <link rel="stylesheet" type="text/css" href="css/style.css">
+        <title>Maxima</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="css/reset.css">
+       	<link rel="stylesheet" type="text/css" href="css/style.css">
+       	
     </head>
     <body>
-        <header class="index-section">
-            <nav class="index-nav">
-                <a class="index-hover" href="/pz2apica.php">PZ2A Bruce Lee SHARKS pica</a>
-                <a class="index-hover" href="/about.php">About Bruce Lee SHARKS</a>
-                <a class="index-hover" href="/index.php">Iš naujo į piceriją</a>
-            </nav>
-        </header>
-        <h1 class="index-h1">Bruce Lee SHARKS</h1>
-        <?php foreach ($bruce_lee_sharks as $row_key => $row_value): ?>
-            <section class="index-section">
-                <?php foreach ($row_value as $column_key => $column): ?>
-                    <?php if ($column == 0): ?>
-                        <div class="index-flex1 index-image-1"></div>
-                    <?php elseif ($column == 1): ?>
-                        <div class="index-flex1 index-image-2"></div>
-                    <?php elseif ($column == 2): ?>
-                        <div class="index-flex1 index-image-3"></div>
-                    <?php endif; ?>
-                <?php endforeach; ?>
-            </section>
-        <?php endforeach; ?>
+    	<div class="all-content">
+	       <header>
+	       	<img src="css/images/maxima.png">
+	       	<h1>Maxima Group Puslapis</h1>
+	       		<nav>
+	       			<ul>
+	       				<li><a href="#about-us">Apie mus</a></li>
+	       				<li><a href="#products">Produktai</a></li>
+	       				<li><a href="#contacts">Kontaktai</a></li>
+	       			</ul>
+	       		</nav>
+	       </header>
+	       <section class="about" id="about-us">
+	       		<h2>Apie Mus</h2>
+	       		<img class="max2-img" src="css/images/max2.png">
+	       		<p>Didžiausias Baltijos šalių mažmeninės prekybos tinklo istorija pradėta 1992 metais, Vilniuje atidarius kelias parduotuves su skirtingais pavadinimais. Šiandien prekybos tinklo MAXIMA kūrimosi istoriją galima drąsiai vadinti verslo sėkmės istorija, nes po Nepriklausomybės paskelbimo Baltijos šalyse nė viena įmonė nesulaukė tokios finansinės sėkmės, žinomumo visuomenėje ir pirkėjų įvertinimo.</p>
+	       		<p>Mūsų siekis - kad MAXIMA taptų pirkėjams patogiausia parduotuve, todėl patogumui skiriame didžiausią dėmesį. Patogumas mums reiškia tai, kad klientas parduotuvėse randa prekes išdėstytas aiškiai, tvarkingai ir visur vienodai. Pirkėjui patogu, kai kainos ir kokybės santykis nekelia abejonių, todėl galima pasitikint viską pirkti vienoje vietoje.
+	       		</p>
+	       </section>
+	       <section class="products-style" id="products">
+	       		<h2>Produktai</h2>
+	       		<h3>Mėsos Gaminiai</h3>
+	       		<div class="product">
+		       		<img src="css/images/jautiena.jpg">
+		       		<h4>Jautienos patis</h4>
+		       		<h4><span>Kaina: </span>€10</h4>
+	       		</div>
+	       		<div class="product">
+	       			<img src="css/images/vistiena.jpg">
+	       			<h4>Vištienos Šlaunelė</h4>
+		       		<h4><span>Kaina: </span>€15</h4>
+	       		</div>
+	       		<h3>Pieno Produktai</h3>
+	       		<div class="product">
+	       			<img src="css/images/pienas.jpg">
+	       			<h4>Pienas</h4>
+		       		<h4><span>Kaina: </span>€2</h4>
+	       		</div>
+	       		<div class="product">
+	       			<img src="css/images/varske.png">
+	       			<h4>Grudėta Varškė</h4>
+		       		<h4><span>Kaina: </span>€3</h4>
+	       		</div>
+	       </section>
+	       <section class="contact" id="contacts">
+	       		<h2>Kontaktai</h2>
+	       		<div class="maxima-ul-left">
+		       		<ul>
+		       			<li>Maxima Group</li>
+		       			<li>+37060010000</li>
+		       			<li>mail@maxima.lt</li>
+		       		</ul>
+	       		</div>
+	       		<img src="css/images/map.jpg">
+	       </section>
+	       <footer>Copyright © Maxima 2019. All rights reserved.</footer>
+       </div>
     </body>
 </html>
+<!-- ctrl shif k in visual studio -->
